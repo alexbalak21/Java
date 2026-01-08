bcaf6e409ba094bd63ee019157e660f9a3c83508
Alex Balak <alex.balak@outlook.com>
2026-01-08 11:34:50 +0100

    date formating

--- src/date/DateExample.java
+++ src/date/DateExample.java
@@ -3,14 +3,18 @@ package date;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class DateExample {
     public static void main(String[] args) {
         LocalDate currentDate = LocalDate.now();
         LocalDateTime currentDateTime = LocalDateTime.now();
         ZonedDateTime currentZonedDateTime = ZonedDateTime.now();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE, dd MMMM, yyyy");
+
         System.out.println("");
-        System.out.println("Current Date : " + currentDate);
+        System.out.println("Current Date : " + currentDate.format(formatter));
         System.out.println("Current Date and Time: " + currentDateTime);
         System.out.println("Current Zoned Date and Time: " + currentZonedDateTime);
     }
