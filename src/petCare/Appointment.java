8ea73d7d41f62b5c0e285067b0a12d78a2ca1951
Alex Balak <alex.balak@outlook.com>
2026-01-08 16:47:54 +0100

    updating to get the best grade

--- src/petCare/Appointment.java
+++ src/petCare/Appointment.java
@@ -5,29 +5,107 @@
 package petCare;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
+/**
+ * Represents an appointment for a pet in the pet care scheduler.
+ * Includes the type of appointment, scheduled date and time, and optional notes.
+ */
 public class Appointment {
     private String type;
     private LocalDateTime dateTime;
     private String notes;
 
-     public Appointment(String type, LocalDateTime dateTime, String notes) {
+    /**
+     * Constructs a new Appointment with the specified type, date/time, and notes.
+     *
+     * @param type the type of appointment (e.g., vet visit, grooming)
+     * @param dateTime the date and time of the appointment
+     * @param notes optional notes about the appointment
+     */
+    public Appointment(String type, LocalDateTime dateTime, String notes) {
         this.type = type;
         this.dateTime = dateTime;
         this.notes = notes;
     }
 
-     public Appointment(String type, LocalDateTime dateTime) {
+    /**
+     * Constructs a new Appointment with the specified type and date/time, with empty notes.
+     *
+     * @param type the type of appointment
+     * @param dateTime the date and time of the appointment
+     */
+    public Appointment(String type, LocalDateTime dateTime) {
         this(type, dateTime, "");
     }
 
+    /**
+     * Gets the type of the appointment.
+     *
+     * @return the appointment type
+     */
     public String getType() {
         return type;
     }
+
+    /**
+     * Gets the date and time of the appointment.
+     *
+     * @return the appointment date and time
+     */
     public LocalDateTime getDateTime() {
         return dateTime;
     }
+
+    /**
+     * Gets the notes for the appointment.
+     *
+     * @return the appointment notes
+     */
     public String getNotes() {
         return notes;
+    }
+
+    /**
+     * Sets the type of the appointment.
+     *
+     * @param type the new appointment type
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * Sets the date and time of the appointment.
+     *
+     * @param dateTime the new date and time
+     */
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    /**
+     * Sets the notes for the appointment.
+     *
+     * @param notes the new notes
+     */
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    /**
+     * Returns a string representation of the appointment with formatted date/time.
+     *
+     * @return a formatted string of the appointment details
+     */
+    @Override
+    public String toString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        String formattedDateTime = dateTime.format(formatter);
+        return "Appointment{" +
+                "type='" + type + '\'' +
+                ", dateTime='" + formattedDateTime + '\'' +
+                ", notes='" + notes + '\'' +
+                '}';
     }
 }
--- src/petCare/Pet.java
+++ src/petCare/Pet.java
@@ -5,6 +5,11 @@ package petCare;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
+/**
+ * Represents a pet in the pet care scheduler system.
+ * Encapsulates pet details including unique ID, personal information, owner details,
+ * registration date, and a list of associated appointments.
+ */
 public class Pet {
     private int id;
     private String name;
@@ -15,6 +20,17 @@ public class Pet {
     private LocalDate registration_date;
     private ArrayList<Appointment> appointments;
 
+    /**
+     * Constructs a new Pet with the specified details.
+     * Registration date is set to the current date, and appointments list is initialized empty.
+     *
+     * @param id the unique identifier for the pet
+     * @param name the name of the pet
+     * @param species_breed the species or breed of the pet
+     * @param age the age of the pet in years
+     * @param owner_name the name of the pet's owner
+     * @param owner_contact_info the contact information of the pet's owner
+     */
     public Pet(int id, String name, String species_breed, int age, String owner_name, String owner_contact_info) {
         this.id = id;
         this.name = name;
@@ -26,51 +42,165 @@ public class Pet {
         this.appointments = new ArrayList<>();
     }
 
+    /**
+     * Gets the unique ID of the pet.
+     *
+     * @return the pet's ID
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Gets the name of the pet.
+     *
+     * @return the pet's name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Gets the species or breed of the pet.
+     *
+     * @return the pet's species or breed
+     */
     public String getSpecies_breed() {
         return species_breed;
     }
 
+    /**
+     * Gets the age of the pet.
+     *
+     * @return the pet's age in years
+     */
     public int getAge() {
         return age;
     }
 
+    /**
+     * Gets the name of the pet's owner.
+     *
+     * @return the owner's name
+     */
     public String getOwner_name() {
         return owner_name;
     }
 
+    /**
+     * Gets the contact information of the pet's owner.
+     *
+     * @return the owner's contact info
+     */
     public String getOwner_contact_info() {
         return owner_contact_info;
     }
 
+    /**
+     * Gets the registration date of the pet.
+     *
+     * @return the registration date
+     */
     public LocalDate getRegistration_date() {
         return registration_date;
     }
 
+    /**
+     * Gets the list of appointments for the pet.
+     *
+     * @return the list of appointments
+     */
     public ArrayList<Appointment> getAppointments() {
         return appointments;
     }
 
+    /**
+     * Adds an appointment to the pet's list.
+     *
+     * @param appointment the appointment to add
+     */
     public void addAppointment(Appointment appointment) {
         this.appointments.add(appointment);
     }
 
+    /**
+     * Sets the unique ID of the pet.
+     *
+     * @param id the new ID
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Sets the name of the pet.
+     *
+     * @param name the new name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Sets the species or breed of the pet.
+     *
+     * @param species_breed the new species or breed
+     */
+    public void setSpecies_breed(String species_breed) {
+        this.species_breed = species_breed;
+    }
+
+    /**
+     * Sets the age of the pet.
+     *
+     * @param age the new age
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * Sets the name of the pet's owner.
+     *
+     * @param owner_name the new owner name
+     */
+    public void setOwner_name(String owner_name) {
+        this.owner_name = owner_name;
+    }
+
+    /**
+     * Sets the contact information of the pet's owner.
+     *
+     * @param owner_contact_info the new contact info
+     */
+    public void setOwner_contact_info(String owner_contact_info) {
+        this.owner_contact_info = owner_contact_info;
+    }
+
+    /**
+     * Sets the registration date of the pet.
+     *
+     * @param registration_date the new registration date
+     */
     public void setRegistrationDate(LocalDate registration_date) {
         this.registration_date = registration_date;
     }
 
+    /**
+     * Sets the list of appointments for the pet.
+     *
+     * @param appointments the new list of appointments
+     */
+    public void setAppointments(ArrayList<Appointment> appointments) {
+        this.appointments = appointments;
+    }
 
+
+    /**
+     * Returns a string representation of the pet.
+     *
+     * @return a string containing all pet details
+     */
     @Override
     public String toString() {
         return "Pet {" +
